fa3a1e30fb67f74a11a073e2f732637480d7b419
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-29 17:08:15 +0530

    Update ColourSelector.js

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const ColourSelector = ({ config, selectNextBackground }) => {
-  const { background, classname, label, key } = config;
+  const { background, classname, label, key: colorKey } = config; // rename key to avoid React key shadowing
 
   return (
     <button
-      className={classname}            
-      data-testid={key}               
+      className={classname}       // button styling
+      data-testid={colorKey}      // Cypress will use this
       onClick={() => selectNextBackground({ background })}
     >
-      {label}                         // display the color label
+      {label}                     {/* display the color label */}
     </button>
   );
 }
